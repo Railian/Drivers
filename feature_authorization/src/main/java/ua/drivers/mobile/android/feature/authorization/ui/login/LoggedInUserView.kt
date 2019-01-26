d3633282510a8ddeb020f79fc1e9a93000460acf
Yevhen Railian <>
2019-01-26 23:22:25 +0200

    Authorization feature - add sample implementation for auth presentation

---
+++ feature_authorization/src/main/java/ua/drivers/mobile/android/feature/authorization/ui/login/LoggedInUserView.kt
@@ -0,0 +1,9 @@
+package ua.drivers.mobile.android.feature.authorization.ui.login
+
+/**
+ * User details post authentication that is exposed to the UI
+ */
+data class LoggedInUserView(
+    val displayName: String
+    //... other data fields that may be accessible to the UI
+)
---
+++ feature_authorization/src/main/java/ua/drivers/mobile/android/feature/authorization/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package ua.drivers.mobile.android.feature.authorization.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
